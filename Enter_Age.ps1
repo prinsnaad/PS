b2d58246e6a3a9ddc7004809791bd4456b3b2d83
Niclas Andersson <naad@aec.se>
2014-08-21 20:31:03 +0200

    Enter_Age.ps1
    Added another script.

---
+++ Enter_Age.ps1
@@ -0,0 +1,10 @@
+﻿$age = read-host "Enter age"
+if ($age -lt 18) {
+	Write-Host "you are under 18"
+}	Elseif ($age -lt 55) {
+	Write-Host "you are under 55"
+}	Elseif ($age -lt 55) {
+	Write-Host "you are under .OLD"
+}	else {
+	Write-Host "You are ancient"
+}
