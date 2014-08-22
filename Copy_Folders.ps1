ba09d3f40820658efab736f4785099bdedffa139
Niclas Andersson <naad@aec.se>
2014-08-22 21:58:51 +0200

    Verbose!
    Verbose and -recurse :D

--- Copy_Folders.ps1
+++ Copy_Folders.ps1
@@ -1,3 +1,3 @@
 ﻿$id = Read-Host "User ID?"
 $ServerName = Read-Host "Server Name?"
-Copy-Item -path "\\$ServerName\temp\$id\*" -Destination "C:\användare\$id\" -Force -Recurse
+Copy-Item -path "\\$ServerName\temp\$id\*" -Destination "C:\användare\$id\" -Force -Recurse -verbose
