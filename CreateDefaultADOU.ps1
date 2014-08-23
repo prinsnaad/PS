0ac1c690cb595a6145cde7f155d5423cc568884d
Niclas Andersson <naad@aec.se>
2014-08-23 14:00:54 +0200

    yep

--- CreateDefaultADOU.ps1
+++ CreateDefaultADOU.ps1
@@ -1,12 +1,15 @@
 ﻿$company = Read-Host "What is the company name?"
 $domain = Read-Host "What is the domain name? Enter as DC=domain,DC=local"
+import-module ActiveDirectory
 New-ADOrganizationalUnit -Name "Common"
 New-ADOrganizationalUnit -Name "$Company"
-NEW-ADOrganizationalUnit “Admins” –path “OU=Common,$domain”
-NEW-ADOrganizationalUnit “Consultants” –path “OU=Common,$domain”
-NEW-ADOrganizationalUnit “Service” –path “OU=Common,$domain”
-NEW-ADOrganizationalUnit “Security” –path “OU=Common,$domain”
-NEW-ADOrganizationalUnit “Distribution” –path “OU=Common,$domain”
-NEW-ADOrganizationalUnit “Users” –path “$company,$domain”
-NEW-ADOrganizationalUnit “Clients” –path “$company,$domain”
-NEW-ADOrganizationalUnit “Servers” –path “$company,$domain”
+NEW-ADOrganizationalUnit “Admins” –path “OU=Common,$domain” > $null
+NEW-ADOrganizationalUnit “Consultants” –path “OU=Common,$domain” > $null
+NEW-ADOrganizationalUnit “Service” –path “OU=Common,$domain” > $null
+NEW-ADOrganizationalUnit “Security” –path “OU=Common,$domain” > $null
+NEW-ADOrganizationalUnit “Distribution” –path “OU=Common,$domain” > $null
+NEW-ADOrganizationalUnit “Users” –path “OU=$company,$domain” > $null
+NEW-ADOrganizationalUnit “Clients” –path “OU=$company,$domain” > $null
+NEW-ADOrganizationalUnit “Servers” –path “OU=$company,$domain” > $null
+redircmp "OU=Clients,OU=$company,$domain" > $null
+redirusr "OU=Users,OU=$company,$domain" > $null
---
+++ New_VM.ps1
@@ -0,0 +1,6 @@
+﻿$name = Read-Host "VM Name"
+$memory = Read-Host "Memory"
+$disksize = Read-Host "Disksize"
+
+
+New-VM -name "$name" -MemoryStartupBytes $memory -NewVHDPath '$name_C.vhdx' -NewVHDSizeBytes $disksize -SwitchName NA-Net
