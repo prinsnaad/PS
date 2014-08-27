0c2c9b9b75b8bb6d29c60a96cd862445c7695f28
Niclas Andersson <naad@aec.se>
2014-08-27 15:57:24 +0200

    Find and copy
    Foreach learned!

---
+++ Find and copy.ps1
@@ -0,0 +1,6 @@
+﻿#Find and copy
+
+$files = Get-ChildItem -path e:\test\folder1 -include "*.txt" -Recurse
+foreach ($file in $files) {
+	Copy-Item -path $file.FullName -Destination e:\test\folder2\
+}
